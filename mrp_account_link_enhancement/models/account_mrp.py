d1b7e9319b00d6b8d6eff43703a3b69e7ef1c7cd
SantiO <santiago.orozco@trescloud.com>
2016-04-19 17:44:34 -0500

    [IMP] mrp_account_link_enhancement: Traducciones

--- mrp_account_link_enhancement/models/account_mrp.py
+++ mrp_account_link_enhancement/models/account_mrp.py
@@ -23,26 +23,46 @@
 #
 ##############################################################################
 
-
+from tools.translate import _
 from openerp.osv import osv, fields
 
 
 class mrp_production(osv.Model):
     
-    _inherit = "mrp.production"
+    _name = 'mrp.production'
+    _inherit = ['mail.thread', "mrp.production"]
     
     def action_account_reprocess(self, cr, uid, ids, context=None):
+        '''
+        Reprocesamiento de asientos contables de ordenes de produccion
+        :param cr: Cursor de la base de datos
+        :param uid: Id de usuario para la transaccion
+        :param ids: Ids de las ordenes de produccion
+        :param context: Variables de contexto como zona horaria, lenguaje, etc
+        '''
         account_move_line_obj = self.pool.get('account.move.line')
         account_move_obj = self.pool.get('account.move')
         stock_move_obj = self.pool.get('stock.move')
+        # Buscamos las lineas los asientos contables asociados a la orden de produccion
         journal_items_ids = account_move_line_obj.search(cr, uid, [('production_id','in', ids if isinstance(ids, (list,tuple)) else [ids])],context=context)
+        # Buscamos los asientos contables asociados a la orden de produccion
         journal_entries_ids = list(set([journal_item.move_id.id for journal_item in account_move_line_obj.browse(cr, uid, journal_items_ids, context=context)]))
+        # Eliminamos los apuntes contables
         account_move_line_obj.unlink(cr, uid, journal_items_ids, context=context)
+        # Cancelamos los asientos contables
         account_move_obj.button_cancel(cr, uid, journal_entries_ids, context=context)
+        # Eliminamos los asientos contables
         journal_entries_ids = account_move_obj.unlink(cr, uid, journal_entries_ids, context=context)
+        # Regeneramos los asientos contables y los apuntes
         for production in self.browse(cr, uid, ids, context=context):
             for move in production.move_lines2:
                 stock_move_obj._create_product_valuation_moves(cr, uid, move, context=context)
+            message = _("The production order number %s has been reprocessed by %s." + 
+                        "\n\nThe cost of the product must be updated manually by the users." if production.state == 'done' else '') %(production.name, self.pool.get('res.users').browse(cr, uid, uid, context=context).name)
+            self.message_post(cr, uid, production.id,
+                        body=message,
+                        subtype='mail.mt_comment',
+                        context=context)
         return True
     
 
