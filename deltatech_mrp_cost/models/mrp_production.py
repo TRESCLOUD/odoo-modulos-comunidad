0ecea17c5f9b1296cf4d849faed8b068c0afa95f
Andres Calle <andres.calle@trescloud.com>
2018-01-07 19:46:09 -0500

    [FIX] Agregado domain vacio cuando no hay registros

--- deltatech_mrp_cost/models/mrp_production.py
+++ deltatech_mrp_cost/models/mrp_production.py
@@ -145,6 +145,7 @@ class MrpProduction(models.Model):
         for move in self.move_finished_ids:
             pickings |= move.picking_id
         action = self.env.ref('stock.action_picking_tree_all').read()[0]
+        action['domain'] = "[('id','in',[0] )]" #if no pickings the domain excludes them all
         if pickings:
             action['domain'] = "[('id','in'," + str(pickings.ids) + " )]"
         return action
