52d3ca98522134b2fcb3fc5f964d49fe1d15811c
Reinier Quevedo Batista <reisyreinier@gmail.com>
2019-10-10 12:22:37 -0400

    [ADD] auth_oauth_disable_login_with_odoo: Se agrega el módulo procedente de https://github.com/onesteinbv/addons-onestein
    Change-Id: I169ccf20dfb1dbb45721de8951498d3de7e907ac

---
+++ auth_oauth_disable_login_with_odoo/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'OAuth2 Disable Login with Odoo.com',
+    'version': '10.0.1.0.0',
+    'category': 'Tools',
+    'author': 'Onestein',
+    'license': 'AGPL-3',
+    'depends': ['auth_oauth'],
+    'data': [
+        'data/auth_oauth_data.xml',
+    ],
+}
